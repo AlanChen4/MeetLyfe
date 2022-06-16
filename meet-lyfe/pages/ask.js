1ba6642a6859093924c038caa44868d210abb8e0
Alan Chen <avchen4@gmail.com>
2022-06-16 17:42:50 -0400

    Add pages for ask, connect, explore, and landing. Also remove boilerplate in index

---
+++ meet-lyfe/pages/ask.js
@@ -0,0 +1,7 @@
+export default function Ask() {
+    return (
+        <div>
+            <h1>Ask</h1>
+        </div>
+    )
+}
---
+++ meet-lyfe/pages/connect.js
@@ -0,0 +1,7 @@
+export default function Connect() {
+    return (
+        <div>
+            <h1>Connect</h1>
+        </div>
+    )
+}
