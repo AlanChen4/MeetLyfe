694050c5711b5b2987357e945792bb85b1d8a1ec
Alan Chen <avchen4@gmail.com>
2022-06-22 16:12:35 -0400

    Add bootstrap and boiler code for profile section

--- meet-lyfe/pages/_app.js
+++ meet-lyfe/pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head';
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+        <meta className="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
