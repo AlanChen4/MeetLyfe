c88293d5a80408d196b96e37d718e9ea780e937e
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2022-06-22 16:12:32 -0400

    added header component and also problem with state

---
+++ meet-lyfe/pages/Header.js
@@ -0,0 +1,22 @@
+export default function Header()
+{
+    return (
+        <div id="header" className="light">
+            <div id="aceimg" className="light">
+                <a href="ask"><img src="./images/acelogo.png" height="75" alt="Logo"></img></a>
+            </div>
+            <div id="ask" className="tab selected light">
+                <a href="ask">Ask</a>
+            </div>
+            <div id="connect" className="tab light">
+                <a href="connect">Connect</a>
+            </div>
+            <div id="explore" className="tab light">
+                <a href="explore">Explore</a>
+            </div>
+            <div id="pfp" className="light">
+                <img src="./images/mikepfp.png" height="50" alt="PFP"></img>
+            </div>
+        </div>
+    );
+}
--- meet-lyfe/pages/ask.js
+++ meet-lyfe/pages/ask.js
@@ -1,34 +1,97 @@
-export default function Ask() {
+//imported from api
+import Header from './Header';
+import { useState, useEffect } from 'react';
+
+//local vars
+let questions = {
+1: {
+    title : 'What truly, really, is JavaScript?',
+    author : 'Michael Phillips',
+    body : "I don't understand what JavaScript is. Is it a language or something?",
+    a : true,
+    replies : [{author : 'Brandon Kaminski', reply : 'yes lmao get good'}],
+},
+2: {
+    title : 'Why are we here?',
+    author : 'Michael Phillips',
+    body : "Just to suffer? Nah, we're vibing",
+    a: true,
+    replies : [],
+},
+3: {
+    title : 'What is this?',
+    author : 'Brandon Kaminski',
+    body : "Anyone know?",
+    a : false,
+    replies : [],
+},
+4: {
+    title : 'MetLife is how old?',
+    author : 'Alan Chen',
+    body : "I was trying to figure it out the other day.",
+    a : false,
+    replies : [{author: 'Brandon Kaminski', reply: 'it was founded in 1868'}],
+},
+}
+
+// function id(n)
+// {
+// return document.getElementById(n);
+// }
+
+//page render
+export default function Ask()
+{
+    //init setup
+    let x = Object.keys(questions).sort(function(a,b){return b - a});
+    //state vars
+    const [currentQ, setCurrentQ] = useState(x[0]);
+
+
+    function getQlist()
+    {
+        let qstr = '';
+        qstr = x.map(n => writeNavHeading(n));
+        console.log(qstr);
+        return <div id = 'qlist'>{qstr}</div>
+    }
+
+    //functions
+    function writeNavHeading(num)
+    {
+        let q = questions[num];
+        let jsx = <p id = {'q' + num} key = {'q' + num} className = "q light" onClick = {function() {displayQ(currentQ)}}> #{num}: {q.title}</p>;
+        return jsx;
+    }
+
+    function displayQ(num)
+    {
+        useEffect(() => {
+            setCurrentQ(num);
+          }, []);
+        console.log(currentQ);
+        let q = questions[num];
+        return(
+            <div id = "askcontainer">
+                <h2>#{num}: {q.title}</h2>
+                <p className="qpar">{q.body}</p>
+                <p className="author">Asked by {q.a ? 'anonymous' : q.author}</p>
+                {q.replies.map(r => <div className="reply"><p><b>{r.author}</b> says:</p> <p>{r.reply}</p></div>)}
+            </div>
+        );
+    }
+
     return (
         <div id = "fullbody">
-            <div id="header" class="light">
-                <div id="aceimg" class="light">
-                    <a href="./ask.html"><img src="./images/acelogo.png" height="75" alt="Logo"></img></a>
-                </div>
-                <div id="ask" class="tab selected light">
-                    <a href="ask.html">Ask</a>
-                </div>
-                <div id="connect" class="tab light">
-                    <a href="ask.html">Connect</a>
-                </div>
-                <div id="explore" class="tab light">
-                    <a href="ask.html">Explore</a>
-                </div>
-                <div id="pfp" class="light">
-                    <img src="./images/mikepfp.png" height="50" alt="PFP"></img>
-                </div>
-            </div>
+            <Header/>
             <div id="qbar">
                 <div>
                     <h2>Questions</h2>
-                    <div id = "qlist">
-                    </div>
+                    {getQlist()}
                 </div>
             </div>
-            <div id="mainask" class="main light">
-                <div id="askcontainer">
-                    
-                </div>
+            <div id="mainask" className="main light">
+                {displayQ(currentQ)}
             </div>
         </div>
     )
