d1f6cf79da23157a3a2eb1e71642dd388bbef368
Alan Chen <avchen4@gmail.com>
2022-07-14 01:44:56 -0400

    Rewrite ask page using hooks and separation into components

---
+++ meet-lyfe/components/Questions.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Questions(props) {
+  const questions = props.questions;
+
+  // search questions by their respective questionId
+  const getQuestionById = (questionId) => {
+    for (const question of questions) {
+      if (question.id == questionId) {
+        return question;
+      }
+    }
+  }
+
+  // author is the person replying, questionId is of the question being responded to
+  const replyQuestion = (reply, author, questionId) => {
+    // TODO: setup auth to determine correct author
+    const replyData = {
+      'reply': reply,
+      'author': 'TEMP_AUTHOR',
+      'questionId': questionId,
+    }
+    axios.post('/api/reply/', replyData);
+  }
+
+  // question ID sets the question in focus for the main container
+  const [questionId, setQuestionId] = useState(questions.length);
+
+  // used to keep track of response
+  const [reply, setReply] = useState('');
+
+  // used to keep track of current question in focus
+  const [currQuestion, setCurrQuestion] = useState();
+
+  // update current question in focus each time questionId changes
+  useEffect(() => {
+    setCurrQuestion(getQuestionById(questionId));
+  }, [questionId])
+
+  return (
+   <div className="row">
+     {/* Sidebar containing questions */}
+     <div className="col-md-3">
+       <h2 className="text-center p-3">Questions</h2>
+       <div className="list-group list-group-flush border-bottom overflow-auto">
+         {questions.map((item, index) => {
+           return (
+             <div className="list-group-item list-group-item-action py-3 lh-tight" key={index} onClick={() => {setQuestionId(item.id)}}>
+              <div className="d-flex w-100 align-items-center">
+                <strong className="mb-1">{item.title}</strong>
+              </div>
+              <small className="ms-auto">{item.author}</small>
+             </div>
+           )
+         })}
+       </div>
+     </div>
+
+     {/* Main content showing question in focus */}
+     <div className="col-md-9 p-3">
+       {/* details related to question in focus */}
+       <div className="mb-3">
+         <h2 className="mb-3">{currQuestion ? currQuestion.title : ''}</h2>
+         <p>{currQuestion ? currQuestion.body : ''}</p>
+         <p className="mb-3">Asked by {currQuestion ? currQuestion.author : ''}</p>
+         <textarea className="form-control w-75" placeholder="Type your reponse here..." onChange={(event) => setReply(event.target.value)}/>
+         <button className="btn btn-dark" onClick={() => replyQuestion(reply, '', questionId)}>Reply</button>
+       </div>
+       {/* reply thread */}
+       <div className="mb-3">
+         <p><strong>Replies ({currQuestion ? currQuestion.replies.length : ''})</strong></p>
+         {currQuestion ? currQuestion.replies.map((reply, index) => {
+           return (
+             <div className="card p-3 mb-2 w-75" key={index}>
+               <p><strong>{reply.author} said</strong></p>
+               <p>{reply.reply}</p>
+             </div>
+           )
+         }) : ''}
+       </div>
+     </div>
+   </div>
+  )
+}
--- meet-lyfe/pages/ask.js
+++ meet-lyfe/pages/ask.js
@@ -1,146 +1,30 @@
-//imported from api
-import Header from '../components/Header';
-import Navbar from '../components/Navbar';
-import { data } from './data';
-import axios from 'axios';
-import react from 'react';
-import ReactDOMServer from 'react-dom/server';
-import path from 'path';
-//page render
-export default function Ask()
-{ 
-    //current q text
-    let qtext = '';
-    //init setup
-    let x = data.sort(function(a,b){return b.id - a.id});
-    function getQlist()
-    {
-        let qstr = '';
-        qstr = x.map(n => writeNavHeading(n.id));
-        return <div id = 'qlist'>{qstr}</div>
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loading from '../components/Loading';
+import Questions from "../components/Questions";
+import Navbar from "../components/Navbar";
+
+
+export default function Ask() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function getQuestions() {
+      await axios.get('/api/questions/')
+        .then((res) => {
+          setQuestions(res.data);
+        });
     }
+    getQuestions();
+  }, [])
 
-    //functions
-    function writeNavHeading(num)
-    {
-        let q = x.find(f => f.id == num);
-        let jsx = <p id = {'q' + num} key = {'q' + num} className = "q light" onClick = {function() {getQ(num)}}> #{num}: {q.title}</p>;
-        return jsx;
-    }
-
-    function getQ(num)
-    {
-        let y = {};
-        axios.get('/api/questions/' + num, {})
-            .then(function (response) {
-            y = response.data;
-            document.getElementById('mainask').innerHTML = ReactDOMServer.renderToStaticMarkup(displayQ(num));
-            }
-        );    
-        
-    }
-
-    function sendNewQ()
-    {
-        //TODO do tags
-        console.log('triggered method');
-        let newqtext = document.getElementById('newqtext').value;
-        let toSend = {
-            title : document.getElementById('newqtitle').value,
-            a : document.getElementById('anonycheck').value,
-            body : newqtext,
-            author : 'Brandon Kaminski',
-            replies : [],
-        }
-        if(newqtext.length > 0)
-        {
-            axios.post('/api/questions/', toSend)
-                .then(function (response) {
-                    console.log(response);
-                }
-            ); 
-        }
-    }
-
-    function sendNewR(num)
-    {
-        //TODO do tags
-        
-        let replytext = document.getElementById('thisreply').value;
-        console.log('triggered method', num);
-        console.log(replytext);
-        if(replytext.length > 0)
-        {
-            let gets = {author: 'Brandon Kaminski', reply: replytext}
-            axios.post('/api/questions/' + num, gets)
-                .then(function (response) {
-                    console.log(response.data);
-                }
-            );
-        }
-        console.log(path.join(process.cwd(), 'data/data.json'))
-        // const fileName = '../data/file.json';    
-        // const file = require(fileName);   
-        // console.log(file);
-    }
-
-    function newQuestion()
-    {
-       let jsx = <div id = "askcontainer">
-                <h2>New Question</h2>
-                <span><input type="checkbox" id="anonycheck"/> anonymous</span>
-                <br/>
-                <input type="text" id = "newqtitle" placeholder="Title of question"/>
-                <br/>
-                <textarea id = "newqtext" placeholder="Type question here..." className='replyarea'/>
-                <br/>
-                <button className = "btn primary" id = 'sendQ' onClick = {function() {sendNewQ()}}>Send</button>
-            </div>
-         document.getElementById('mainask').innerHTML = ReactDOMServer.renderToStaticMarkup(jsx);
-         
-    }
-
-
-    function displayQ(num)
-    {
-        let q = x.find(f => f.id == num);
-        return(
-            <div id = "askcontainer">
-                <h2>#{num}: {q.title}</h2>
-                <p className="qpar">{q.body}</p>
-                <p className="author">Asked by {q.a ? 'anonymous' : q.author}</p>
-                
-                <textarea id='thisreply' placeholder = {q.a ? 'Reply here...' : `Reply to ${q.author} here...`} className='replyarea'/>
-                <br/>
-                <button onClick={function() {sendNewR(num)}} className='btn btn-primary mb-4'>Send</button>
-                <p id='replies'>Replies ({q.replies.length})</p>
-                {q.replies.map((r, index) => 
-                <div key={'reply' + q.id + '-' + index} className="card mb-2">
-                    <div className = 'card-body'>
-                        <p className = "card-text"><b>{r.author}</b> says:<br/>{r.reply}</p>
-                    </div>
-                </div>
-                )}
-                
-            </div>
-        );
-    }
-
-    return (
-        <div id = "fullbody">
-            <Navbar/>
-            <div className = "row">
-                <div id="qbar" className="col-3">
-                    <div>
-                        <h2>Questions</h2>
-                        <p id = 'newqbar' className = 'q light' onClick={function() {newQuestion()}}>New Question...</p>
-                        {getQlist()}
-                    </div>
-                </div>
-                <div id="mainask" className="main light col-9">
-                    {displayQ(x[0].id)}
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      {questions.length <= 0
+        ? <Loading />
+        : <Questions questions={questions} />
+      }
+    </div>
+  )
 }
