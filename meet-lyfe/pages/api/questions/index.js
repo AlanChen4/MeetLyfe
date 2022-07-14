3119bca5aa0f2ceaa3e360d5b89a8023dc05e051
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2022-07-14 15:04:23 -0400

    moved to questions.js

--- meet-lyfe/pages/api/questions/index.js
+++ meet-lyfe/pages/api/questions/index.js
@@ -7,7 +7,24 @@ export default function handler(req, res) {
   }
 
   else if (req.method == 'POST') {
+     // extract request data from body
+     const reqData = req.body;
+
+     // add qdata from the POST request
+     const updated = questions.push({
+       "id": questions.length + 1,
+       "title": reqData.title,
+       "author": "TEMP_AUTHOR",
+       "body": reqData.body,
+       "a": reqData.a,
+       "replies": []
+     });
+     
+     // write changes to json file
+     fs.writeFileSync('./pages/api/questions.json', JSON.stringify(updated, null, 4));
+ 
+     res.status(200).json({ message: 'success' });
     // TODO: write new reply data to json file 
-    res.status(200).json({ message: 'Added new reply' });
+    res.status(200).json({ message: 'Added new question' });
   }
 }
