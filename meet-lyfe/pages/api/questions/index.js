4c09272e2994d5607b4f885212e566deb065257b
Alan Chen <avchen4@gmail.com>
2022-07-14 01:45:49 -0400

    Add endpoints to handle GET/POST for questions and replies

--- meet-lyfe/pages/api/questions/index.js
+++ meet-lyfe/pages/api/questions/index.js
@@ -1,39 +1,13 @@
-const fsp = require('fs').promises;
-const path = require('path');
-export default async function (req, res) {
-  try {
-    const file_data = await fsp.readFile(path.join(process.cwd(), 'data/data.json'))//path.join(path.dirname((__dirname)), 'data.json'))
-    const json_data = JSON.parse(file_data);
-    // Do stuff
-    if (method === "GET") {
-        return res.status(200).json(json_data);
-    }
+const fs = require('fs');
+const questions = require('../questions.json');
 
-    if (method === "POST") {
-           const { body } = request;
-           data.push({ ...body, id: data.length + 1 });
-           return response.status(200).json(json_data);
-    }
+export default function handler(req, res) {
+  if (req.method == 'GET') {
+    res.status(200).json(questions);
   }
-  catch (error) {
-    console.log(error)
-    res.status(500).json({ err: `${error}` })
+
+  else if (req.method == 'POST') {
+    // TODO: write new reply data to json file 
+    res.status(200).json({ message: 'Added new reply' });
   }
-} 
-
-
-// import {data} from '../../data.js';
-
-// export default function handler(request, response) {
-//   const { method } = request;
-
-//   if (method === "GET") {
-//     return response.status(200).json(data);
-//   }
-
-//   if (method === "POST") {
-//     const { body } = request;
-//     data.push({ ...body, id: data.length + 1 });
-//     return response.status(200).json(data);
-//   }
-// }
+}
---
+++ meet-lyfe/pages/api/reply/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const questions = require('../questions.json');
+
+export default function handler(req, res) {
+  if (req.method == 'POST') {
+    // extract request data from body
+    const reqData = req.body;
+
+    // add reply from the POST request
+    for (const question of questions) {
+      if (question.id == reqData.questionId) {
+        question.replies.push({reply: reqData.reply, author: reqData.author});
+      }
+    }
+    
+    // write changes to json file
+    fs.writeFileSync('./pages/api/questions.json', JSON.stringify(questions, null, 4));
+
+    res.status(200).json({ message: 'success' });
+  }
+}
