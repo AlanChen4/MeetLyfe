86ee5742e3137086cf44ab61e299ee1c3cae1a0e
Alan Chen <avchen4@gmail.com>
2022-07-19 19:05:48 -0400

    Add rows for layout

--- meet-lyfe/pages/explore.js
+++ meet-lyfe/pages/explore.js
@@ -4,7 +4,15 @@ export default function Explore() {
     return (
         <div>
         <Navbar/>
-        <div><h1>Explore</h1></div>
-    </div>
+            <div className="row p-4">
+                <h2 className="text-center">Michael's Up Next</h2>
+            </div>
+            <div className="row p-4">
+                <h2 className="text-center">Happening @ MetLife</h2>
+            </div>
+            <div className="row p-4">
+                <h4>Today</h4>
+            </div>
+        </div>
     )
 }
